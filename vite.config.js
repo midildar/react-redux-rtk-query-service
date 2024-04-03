316968bbe40e43f2866f1ad0b3ca4b8de13b69c5
Ismaeel <ismaeel.dildar98@gmail.com>
2024-04-04 00:00:00 +0500

    Commit: vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+# Random change 23739
+# Random change 32213
+# Random change 5364
+# Random change 12192
+# Random change 25103
+# Random change 30746
+# Random change 13434
+# Random change 19497
