27bbf04029dc76ea336a65bf3134e99af98c1a04
Ismaeel <ismaeel.dildar98@gmail.com>
2024-06-09 00:00:00 +0500

    Commit: .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
+# Random change 31051
+# Random change 12137
+# Random change 19091
+# Random change 10049
+# Random change 14713
+# Random change 2531
+# Random change 3398
+# Random change 18867
