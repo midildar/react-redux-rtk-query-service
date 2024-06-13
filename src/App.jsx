3c9e3ba8afa4625332c192dbdeaf6127cf4bef93
Ismaeel <ismaeel.dildar98@gmail.com>
2024-06-14 00:00:00 +0500

    Commit: App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import AllProducts from './components/AllProducts'
+import SpecificProduct from './components/SpecificProduct'
+import AddNewProduct from './components/AddNewProduct'
+import UpdateProduct from './components/UpdateProduct'
+import DeleteProduct from './components/DeleteProduct'
+
+const App = () => {
+  return (
+    <>
+      {/* <AllProducts/> */}
+      {/* <SpecificProduct/> */}
+      {/* <AddNewProduct/> */}
+      {/* <UpdateProduct productId={4}/> */}
+      <DeleteProduct productId={4}/>
+    </>
+  )
+}
+
+export default App# Random change 7409
+# Random change 8220
+# Random change 25566
+# Random change 31839
+# Random change 7207
+# Random change 15194
+# Random change 28258
+# Random change 29898
