61f0feec3612f39a475b51f7d0fb720ed625722f
Ismaeel <ismaeel.dildar98@gmail.com>
2024-06-18 00:00:00 +0500

    Commit: main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {store} from './app/store.js'
+import {Provider} from 'react-redux'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+)
+# Random change 18420
+# Random change 19816
+# Random change 25647
+# Random change 25438
+# Random change 336
+# Random change 17758
+# Random change 17824
+# Random change 6012
